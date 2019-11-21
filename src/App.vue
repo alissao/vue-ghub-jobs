b5983b1394ca0dd39d456ce0d63c0779a2c2b983
alissao <danvil54@hotmail.com>
2019-11-20 22:03:45 -0300

    Traded primefaces or materialize

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,81 @@
 <template>
   <div id="app">
-    <GithubJobs>
 
-    </GithubJobs>
+    <nav>
+      <div class="nav-wrapper blue darken-1">
+        <a href="#" class="brand-logo center">Github Jobs with Materialize and Vue</a>
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <form>
+
+          <label>Job Description</label>
+          <input type="text" placeholder="Job Description">
+          <label>Location</label>
+          <input type="text" placeholder="Location">
+          <label>Full Time</label>
+          <p>
+            <label>
+              <input class="with-gap" name="Full time" type="radio" checked />
+              <span>Yes</span>
+            </label>
+            <label>
+              <input class="with-gap" name="Full time" type="radio" />
+              <span>No</span>
+            </label>
+          </p>
+
+          <button class="waves-effect waves-light btn-small">Pesquisar<i class="material-icons left">search</i></button>
+
+      </form>
+
+      <table>
+
+        <thead>
+
+          <tr>
+            <th>NOME</th>
+            <th>QTD</th>
+            <th>VALOR</th>
+            <th>OPÇÕES</th>
+          </tr>
+
+        </thead>
+
+        <tbody>
+
+          <tr>
+
+            <td>Arduino</td>
+            <td>100</td>
+            <td>50.00</td>
+            <td>
+              <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
+              <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
+            </td>
+
+          </tr>
+
+        </tbody>
+      
+      </table>
+
+    </div>
+
   </div>
 </template>
 
 <script>
-import GithubJobs from './components/GithubJobs.vue'
 
-export default {
-  name: 'app',
-  components: {
-    GithubJobs
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 30px;
-}
-
 @media (min-width: 1200px) {
   .container {
     width: 1170px;
-    margin: 0 auto;
+    margin: 10 auto;
   }
 }
 </style>
