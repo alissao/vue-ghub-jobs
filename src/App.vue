84162764bd12e376733fa47031cb50106fabbf36
alissao <danvil54@hotmail.com>
2019-11-21 23:39:20 -0300

    Connected and consumming github jobs API with Axios

--- src/App.vue
+++ src/App.vue
@@ -31,44 +31,79 @@
 
       </form>
 
-      <table>
-
-        <thead>
-
-          <tr>
-            <th>NOME</th>
-            <th>QTD</th>
-            <th>VALOR</th>
-            <th>OPÇÕES</th>
-          </tr>
-
-        </thead>
-
-        <tbody>
-
-          <tr>
-
-            <td>Arduino</td>
-            <td>100</td>
-            <td>50.00</td>
-            <td>
-              <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
-              <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
-            </td>
-
-          </tr>
-
-        </tbody>
+      <div class="row">
+        <div class="col s4" v-for="job in jobs" :key="job.id">
+          <!--h5 style="text-align: center">{{ job.title }}</h5-->
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <img align="right" height="50" width="100" v-bind:src="job.company_logo" />
+              <span class="card-title">{{ job.title }}</span>
+              {{ job.description }}
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <ul class="pagination">
+            <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+            <li class="active"><a href="#!">1</a></li>
+            <li class="waves-effect"><a href="#!">2</a></li>
+            <li class="waves-effect"><a href="#!">3</a></li>
+            <li class="waves-effect"><a href="#!">4</a></li>
+            <li class="waves-effect"><a href="#!">5</a></li>
+            <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+          </ul>
+        </div>
+      </div>
       
-      </table>
-
     </div>
 
   </div>
 </template>
 
 <script>
+import Jobs from './services/jobs'
 
+export default {
+
+  name: 'app',
+  data () {
+    return {
+      job:{
+        id: '',
+        type: '',
+        url: '',
+        created_at: '',
+        company: '',
+        company_url: '',
+        location: '',
+        title: '',
+        description: '',
+        how_to_apply: '',
+        company_logo: ''
+      },
+      jobs: []
+    }
+  },
+
+  mounted(){
+    this.listar()
+  },
+
+  methods:{
+    
+    listar(){
+      Jobs.listar().then(resposta => {
+        this.jobs = resposta.data
+      }).catch(e => {
+        console.log(e)
+      })
+    }
+  }
+}
 </script>
 
 <style>
--- src/services/jobs.js
+++ src/services/jobs.js
@@ -2,8 +2,15 @@ import { http } from './config'
 
 export default {
 
-    listar:() => {
-        return http.get('', )
+    listar:(page, description, location, fulltime) => {
+
+        var pagination = 'page=' & page || 'page=1';
+        var search = '&description=' & description || '';
+        var local = '&location=' & location || '';
+        var ftime = '&full_time=' & fulltime || '';
+
+
+        return http.get(pagination & search & local & ftime)
     }
 
 }
